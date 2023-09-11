5578520df00cf0abf2f3d3e47a6d1e51083f93e5
Anudeep Y <anudeep.yegireddi@gmail.com>
2023-09-11 23:06:30 +0530

    bug: revert user confirmation.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from indexer import BuildRagIndex, index_to_product_mapping, product_description
 
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+import json
 
 import logging
 
@@ -34,53 +35,67 @@ query = "Something is wrong with the scanner. What should I do?"
 
 memory_conv_id = 0
 
+global_memory: Message | None = None
+
+
+def memory_refresher():
+    with open("memory.txt", "w") as f:
+        f.write("None")
+
+
+def memory_getter() -> Message | None:
+    f = open("memory.txt", "r")
+    memory = f.read()
+    f.close()
+    if memory == "None":
+        # return ready to restart
+        return None
+    else:
+        memory = json.loads(memory)
+        return Response(**memory)
+
 
 @app.get("/converse/")
 def get_response(
     message: Message,
-) -> Response:
-    memory: Message | None = None
-
+) -> dict | Response:
     logger.info(message.content)
 
-    if memory is None:
-        # means this is a fresh request
-        # send a classification response
-        return get_classification(message)
-        ...
-    elif memory.content.lower() in ["", "y", "yes"]:
-        # switch the message so as to reset the memory for the next call
-        past_memory = memory.deepcopy()
-        memory = None
-        # perform the rag call
-        return perform_rag_call(past_memory)
+    # attempt at adding memory did not succeed, no confirmation possible for now from user
+    # memory = memory_getter()
 
+    # if memory is None:
+    #     print("memory is None, hence doing classification")
+    #     # means this is a fresh request
+    #     # send a classification response
+    #     return get_classification(message)
+    #     ...
+    # elif message.content.lower() in ["", "y", "yes"]:
+    #     # switch the message so as to reset the memory for the next call
+    #     memory_refresher()
+    #     # perform the rag call
+    #     return perform_rag_call(memory)
 
-class ConversationHandler:
-    def __init__(self, message: Message):
-        self.memory: Message | None = None
+    # def get_classification(message: Message) -> Response:
 
+    # product_that_query_is_about = classification_agent(message.content)
+    # product_that_query_is_about = product_that_query_is_about.strip()
 
-def get_classification(message: Message) -> Response:
-    product_that_query_is_about = classification_agent(message.content)
-    product_that_query_is_about = product_that_query_is_about.strip()
+    # logger.debug(f"product_that_query_is_about: {product_that_query_is_about}")
+    # # appropriate rag index
+    # try:
+    #     index_id = index_to_product_mapping[product_that_query_is_about]
+    #     msg1 = f"You seem to be asking about {product_that_query_is_about}.\n\nIf it isn't, try asking the question again with a few more contextual clues.\nI am fairly capable, and I'll figure it out for you."
+    #     return Response(
+    #         response=msg1, product=product_that_query_is_about, sources=None
+    #     )
+    # except KeyError:
+    #     msg1 = f"Sorry, I cannot seem to find the product you are asking about in my database.\n\n"
+    #     msg2 = f"As reference, I only have the following products in my database:\n{list(index_to_product_mapping.keys())}"
+    #     msg3 = f"\n\nPlease try again. It may help to give any identifying infromation about the product for my benefit."
+    #     # return Response(response=msg1 + msg2 + msg3, product=None, sources=None)
 
-    logger.debug(f"product_that_query_is_about: {product_that_query_is_about}")
-    # appropriate rag index
-    try:
-        index_id = index_to_product_mapping[product_that_query_is_about]
-        msg1 = f"You seem to be asking about {product_that_query_is_about}. Press enter if I got it right. \n\nIf not type `no`, and I will try asking the question again.\n\nI am fairly capable, so help me with a few contextual clues and I'll figure it out."
-        return Response(
-            response=msg1, product=product_that_query_is_about, sources=None
-        )
-    except KeyError:
-        msg1 = f"Sorry, I cannot seem to find the product you are asking about in my database.\n\n"
-        msg2 = f"As reference, I only have the following products in my database:\n{list(index_to_product_mapping.keys())}"
-        msg3 = f"\n\nPlease try again. It may help to give any identifying infromation about the product for my benefit."
-        return Response(response=msg1, product=None, sources=None)
-
-
-def perform_rag_call(message: Message) -> Response:
+    # # def perform_rag_call(message: Message) -> Response:
     # response query initialize
     response_query = []
     # find the appropriate index for the product
